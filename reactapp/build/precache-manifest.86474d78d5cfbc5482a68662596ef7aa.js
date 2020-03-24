cd4f8cf8fabab8be5b1e6fa21b699ca293745ff4
DeVante Bailey <devantebailey1322@gmail.com>
2020-03-24 14:40:36 -0400

    fixed deployment

---
+++ reactapp/build/precache-manifest.86474d78d5cfbc5482a68662596ef7aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9505d9c8eec65fe95c8962149490950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536a09ef6677530b7ae5",
+    "url": "/static/css/main.91114ef8.chunk.css"
+  },
+  {
+    "revision": "bef75defaff1262fa7e4",
+    "url": "/static/js/2.7bf98ef2.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.7bf98ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536a09ef6677530b7ae5",
+    "url": "/static/js/main.2579d61c.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "a9a5368ec59fe2cd95f382f769bf297a",
+    "url": "/static/media/devante.a9a5368e.png"
+  },
+  {
+    "revision": "a96db1892e45d80fff7b9ac02656fe02",
+    "url": "/static/media/emokid.a96db189.png"
+  },
+  {
+    "revision": "7300a870b16eff8b24e6c5c1a0e8dbfc",
+    "url": "/static/media/project1.7300a870.png"
+  },
+  {
+    "revision": "60a7f412ee189ff05b7123b3a1f09d37",
+    "url": "/static/media/project2.60a7f412.png"
+  },
+  {
+    "revision": "fe49abb95631f8eb383978eed4a28439",
+    "url": "/static/media/project3.fe49abb9.png"
+  },
+  {
+    "revision": "85d471ac7853e40023dca6b18c67c140",
+    "url": "/static/media/project4.85d471ac.png"
+  },
+  {
+    "revision": "fd2197867c142121e9787f2e2569e6d1",
+    "url": "/static/media/project5.fd219786.png"
+  },
+  {
+    "revision": "0c2fde8d89cb9593264f22111fcb4c8b",
+    "url": "/static/media/project6.0c2fde8d.png"
+  }
+]);
